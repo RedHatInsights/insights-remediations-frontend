c65f95a7335ced5da0c930345a5110a97f3ce78d
Jozef Hartinger <jharting@redhat.com>
2018-10-10 16:20:39 +0200

    set up deployment

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -6,10 +6,10 @@ if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
     echo -e "Pull Request, not pushing a build"
     exit 0;
 else
-    openssl aes-256-cbc -K $encrypted_ca9080c40340_key -iv $encrypted_ca9080c40340_iv -in starter.enc -out starter -d
-    chmod 600 starter
+    openssl aes-256-cbc -K $encrypted_834c6a7e1ce2_key -iv $encrypted_834c6a7e1ce2_iv -in key_rsa.enc -out key_rsa -d
+    chmod 600 key_rsa
     eval `ssh-agent -s`
-    ssh-add starter
+    ssh-add key_rsa
 fi
 
 # If current dev branch is master, push to build repo master
