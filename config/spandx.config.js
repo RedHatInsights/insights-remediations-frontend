f7fce1554f4abf87e3b1c5bdc25e4b714f9231ad
Jozef Hartinger <jharting@redhat.com>
2018-10-12 16:28:06 +0200

    spandx configs

---
+++ config/spandx.config.js
@@ -0,0 +1,5 @@
+/* global exports */
+
+exports.routes = {
+    '/v1/remediations': { host: 'https://remediations-ci.1b13.insights.openshiftapps.com' }
+};
---
+++ config/spandx.config.local.js
@@ -0,0 +1,5 @@
+/* global exports */
+
+exports.routes = {
+    '/v1/remediations': { host: 'http://localhost:9002' }
+};
