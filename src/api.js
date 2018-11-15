2f1a1cb6dd36de4a0ff5762964b612fead726798
Jozef Hartinger <jharting@redhat.com>
2018-11-15 12:45:14 +0100

    send x-rh-insights-use-path-prefix header for now

--- src/api.js
+++ src/api.js
@@ -5,7 +5,12 @@ import urijs from 'urijs';
 export function getRemediations () {
     const uri = urijs(API_BASE).segment('remediations').toString();
 
-    return fetch(uri).then(r => {
+    return fetch(uri, {
+        headers: {
+            // TODO: this is only needed temporarily until the app is properly onboarded
+            'x-rh-insights-use-path-prefix': '1'
+        }
+    }).then(r => {
         if (r.ok) {
             return r.json();
         }
