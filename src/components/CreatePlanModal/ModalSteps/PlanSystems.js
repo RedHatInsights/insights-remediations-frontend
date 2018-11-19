593cb0d7667ea669a7ea3cfaa4f2dbd57de0c8c3
Ryan Long <rlong@redhat.com>
2018-11-19 14:39:57 -0500

    adding progress in table, switching data to match mocks, starting modal dev

---
+++ src/components/CreatePlanModal/ModalSteps/PlanSystems.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+
+import {
+    Form,
+    FormGroup,
+    Radio,
+    Select,
+    SelectOption
+} from '@patternfly/react-core';
+
+import './PlanSystems.scss';
+
+class PlanSystems extends Component {
+
+    constructor () {
+        super();
+        this.state = {
+            systemValue: 'Select a System',
+            groupValue: 'Select a System'
+        };
+        this.onChangeSystemOptions = this.onChangeSystemOptions.bind(this);
+        this.onChangeSystemGroup = this.onChangeSystemGroup.bind(this);
+
+        // TODO: Change these to actual system groups
+        this.systemOptions = [
+            { value: 'Select a System', label: 'Select a System', disabled: true },
+            { value: 'System 1', label: 'System 1', disabled: false },
+            { value: 'System 2', label: 'System 2', disabled: false }
+        ];
+        this.groupOptions = [
+            { value: 'Select a System', label: 'Select a System Group', disabled: true },
+            { value: 'Group 1', label: 'Group 1', disabled: false },
+            { value: 'Group 2', label: 'Group 2', disabled: false }
+        ];
+    };
+
+    onChangeSystemOptions(systemValue) {
+        this.setState({ systemValue });
+    };
+
+    onChangeSystemGroup(groupValue) {
+        this.setState({ groupValue });
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <h2> Select the system(s) for the plan </h2>
+                <Form className='select-systems-form'>
+                    <FormGroup isRequired fieldId="select-systems">
+                        <Radio id="one-system" name="select-systems" label="A System" aria-label="A System" />
+                        <Select value= { this.state.systemValue } onChange={ this.onChangeSystemOptions } aria-label="Select Input">
+                            { this.systemOptions.map((option, index) => (
+                                <SelectOption isDisabled={ option.disabled } key={ index } value={ option.value } label={ option.label } />
+                            )) }
+                        </Select>
+
+                        <Radio id="system-group" name="select-systems" label="System Group" aria-label="System Group" />
+                        <Select value= { this.state.groupValue } onChange={ this.onChangeSystemGroup } aria-label="Select Input">
+                            { this.groupOptions.map((option, index) => (
+                                <SelectOption isDisabled={ option.disabled } key={ index } value={ option.value } label={ option.label } />
+                            )) }
+                        </Select>
+
+                        <Radio id="all-systems" name="select-systems" label="All Systems (x)" aria-label="All Systems" />
+                    </FormGroup>
+                </Form>
+            </React.Fragment>
+        );
+    };
+};
+
+export default PlanSystems;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,12 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import { Main, PageHeader, PageHeaderTitle } from '@red-hat-insights/insights-frontend-components';
+import { Modal, Button } from '@patternfly/react-core';
 import RemediationTable from '../components/RemediationTable';
+
+import CreatePlanModal from '../components/CreatePlanModal/CreatePlanModal.js';
+
+import './Home.scss';
 
 const ConnectedRemediationTable = connect(({ remediations }) => ({ ...remediations }))(RemediationTable);
 
@@ -15,6 +20,32 @@ class Home extends Component {
     constructor (props, ctx) {
         super(props, ctx);
         this.loadRemediations = () => ctx.store.dispatch(actions.loadRemediations());
+        this.state = {
+            isModalOpen: false,
+            modalStep: 0
+        };
+        this.handlePreviousModalStep = this.handlePreviousModalStep.bind(this);
+        this.handleNextModalStep = this.handleNextModalStep.bind(this);
+        this.handleModalToggle = this.handleModalToggle.bind(this);
+    };
+
+    handleModalToggle() {
+        this.setState(({ isModalOpen }) => ({
+            isModalOpen: !isModalOpen,
+            modalStep: 0
+        }));
+    };
+
+    handleNextModalStep() {
+        this.setState(({ modalStep }) => ({
+            modalStep: modalStep + 1
+        }));
+    }
+
+    handlePreviousModalStep() {
+        this.setState(({ modalStep }) => ({
+            modalStep: modalStep - 1
+        }));
     }
 
     componentDidMount () {
@@ -22,14 +53,43 @@ class Home extends Component {
     }
 
     render() {
+
+        const { isModalOpen } = this.state;
+
+        let renderModalActions =  [
+            <Button key="cancel" variant="secondary" onClick={ this.handleModalToggle }>
+            Cancel
+            </Button>,
+            // Conditionally render 'previous' button if not on first page
+            this.state.modalStep > 0
+                ? <Button key="previous" variant="secondary" onClick={ this.handlePreviousModalStep }> Previous </Button>
+                : null,
+            // Conditionally render 'confirm' button if on last page
+            this.state.modalStep < 5
+                ? <Button key="continue" variant="primary" onClick={ this.handleNextModalStep }> Continue </Button>
+                : <Button key="confirm" variant="primary" onClick={ this.handleModalToggle }> Confirm </Button>
+        ];
+
         return (
             <React.Fragment>
                 <PageHeader>
                     <PageHeaderTitle title='Remediations'></PageHeaderTitle>
+                    <Button variant='primary' onClick={ this.handleModalToggle }>Create Plan</Button>
                 </PageHeader>
                 <Main>
                     <ConnectedRemediationTable />
                 </Main>
+
+                <Modal
+                    isLarge
+                    title="Create Plan"
+                    className='ins-c-plan-modal'
+                    isOpen={ isModalOpen }
+                    onClose={ this.handleModalToggle }
+                    actions={ renderModalActions }
+                >
+                    <CreatePlanModal step={ this.state.modalStep }/>
+                </Modal>
             </React.Fragment>
         );
     }
