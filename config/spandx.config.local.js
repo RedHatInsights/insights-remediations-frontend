5c82ee406c80d008086629a8616f4cb325e44d4b
Jozef Hartinger <jharting@redhat.com>
2018-11-19 15:23:57 +0100

    make proxy config mac-friendly

--- config/spandx.config.local.js
+++ config/spandx.config.local.js
@@ -1,5 +1,9 @@
 /* global exports */
 
+const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docker.internal';
+
 exports.routes = {
-    '/r/insights/platform/remediations': { host: 'http://localhost:9002' }
+    '/r/insights/platform/remediations': {
+        host: `http://${localhost}:9002`
+    }
 };
