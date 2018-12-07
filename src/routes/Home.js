a435ad14de0a33f799643894b58a8a21bec146ef
Jozef Hartinger <jharting@redhat.com>
2018-12-07 11:18:01 +0100

    hot-loadable RemediationWizard test bed

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 
 import * as actions from '../actions';
 
-import { Main, PageHeader, PageHeaderTitle, Wizard } from '@red-hat-insights/insights-frontend-components';
+import { Main, PageHeader, PageHeaderTitle, Wizard, RemediationButton } from '@red-hat-insights/insights-frontend-components';
 import { Button } from '@patternfly/react-core';
 import RemediationTable from '../components/RemediationTable';
 import NewRemediationButton from '../components/NewRemediationButton';
@@ -55,6 +55,24 @@ class Home extends Component {
         this.store.dispatch(addNotification(data));
     }
 
+    dataProvider = () => ({
+        issues: [{
+            id: 'vulnerabilities:CVE_2017_6074_kernel|KERNEL_CVE_2017_6074'
+        }, {
+            id: 'advisor:network_bond_opts_config_issue|NETWORK_BONDING_OPTS_DOUBLE_QUOTES_ISSUE'
+        }],
+        systems: [
+            '34b9f7d9-fc81-4e0f-bef0-c4b402a1510e',
+            'faa8c67c-345a-44b3-bb8a-d1bc89c36446',
+            'da8355c0-b259-490d-a9ec-8c1bc0ba7e98'
+        ]
+    });
+
+    onRemediationCreated = result => {
+        this.sendNotification(result.getNotification());
+        this.loadRemediations();
+    };
+
     render() {
 
         const { isModalOpen } = this.state;
@@ -71,6 +89,11 @@ class Home extends Component {
                     <PageHeaderTitle title='Remediations'></PageHeaderTitle>
                     <Button variant='primary' onClick={ this.openModal }>Create Remediation</Button>
                     <NewRemediationButton onCreated = { this.loadRemediations } />
+                    <RemediationButton
+                        dataProvider={ this.dataProvider }
+                        onRemediationCreated={ this.onRemediationCreated } >
+                        Hot-loaded Wizard
+                    </RemediationButton>
                 </PageHeader>
                 <Main>
                     <ConnectedRemediationTable />
