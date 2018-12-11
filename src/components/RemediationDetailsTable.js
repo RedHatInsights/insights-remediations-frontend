a4598abd6ec6fb6d1875200c111f1ef49db903dd
Ryan Long <rlong@redhat.com>
2018-12-11 14:08:45 -0500

    adding table to id page

---
+++ src/components/RemediationDetailsTable.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Table } from '@red-hat-insights/insights-frontend-components';
+import { Level, LevelItem, TextInput, Button, Stack, StackItem, Split, SplitItem } from '@patternfly/react-core';
+import './RemediationTable.scss';
+
+class RemediationDetailsTable extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    onExpandClicked(event, row, rowKey) {
+        const { rows } = this.state;
+        rows[rowKey].isActive = !row.isActive;
+        row.children.forEach(childKey => rows[childKey].isOpen !== rows[childKey].isOpen);
+        this.setState({ rows });
+    }
+
+    render() {
+
+        const { remediation } = this.props;
+
+        const rows = remediation.issues.map(issue => (
+            {
+                cells: [
+                    issue.description,
+                    issue.resolution.description,
+                    issue.resolution.needs_reboot.toString(),
+                    issue.systems.length,
+                    'fix'
+                ]
+            }
+        ));
+
+        return (
+            <Stack gutter="md">
+                <StackItem className='ins-c-remediations-details-table__toolbar'>
+                    <Level>
+                        <LevelItem>
+                            { /* TODO: Fix filtering */ }
+                            <TextInput
+                                type="text"
+                                value= ' '
+                                placeholder="Filter"
+                                aria-label='Filter'
+                            />
+                        </LevelItem>
+                        <LevelItem>
+                            <Split gutter="md">
+                                <SplitItem><Button> Add Action </Button></SplitItem>
+                                <SplitItem><Button> Remove Action </Button></SplitItem>
+                            </Split>
+                        </LevelItem>
+                    </Level>
+                </StackItem>
+                <StackItem>
+                    <Table
+                        header={ [
+                            {
+                                title: 'Actions',
+                                hasSort: true
+                            }, {
+                                title: 'Resolution',
+                                hasSort: true
+                            }, {
+                                title: 'Reboot Required',
+                                hasSort: true
+                            }, {
+                                title: '# of systems',
+                                hasSort: true
+                            }, {
+                                title: 'Type',
+                                hasSort: true
+                            }]
+                        }
+                        hasCheckbox={ true }
+                        rows= { rows }
+                    />
+                </StackItem>
+            </Stack>
+        );
+    };
+}
+
+RemediationDetailsTable.propTypes = {
+    remediation: PropTypes.object
+};
+
+export default RemediationDetailsTable;
--- src/routes/RemediationDetails.js
+++ src/routes/RemediationDetails.js
@@ -22,6 +22,7 @@ import {
     Button
 } from '@patternfly/react-core';
 
+import RemediationDetailsTable from '../components/RemediationDetailsTable';
 import './RemediationDetails.scss';
 
 class RemediationDetails extends Component {
@@ -32,11 +33,9 @@ class RemediationDetails extends Component {
         this.state = {
             autoReboot: true
         };
-
         this.id = this.props.computedMatch.params.id;
         this.loadRemediation = this.props.loadRemediation.bind(this, this.id);
     };
-
     handleRebootChange = autoReboot => {
         this.props.switchAutoReboot(this.id, autoReboot);
     };
@@ -72,75 +71,82 @@ class RemediationDetails extends Component {
                     </Level>
                 </PageHeader>
                 <Main>
-                    <Grid gutter="md" sm={ 12 } md={ 4 } className='ins-c-summary-cards'>
-                        <GridItem>
-                            <Card className='ins-c-card__actions-resolved'>
-                                <CardHeader>
-                                    <Level>
-                                        <LevelItem>
-                                            Actions Resolved
-                                        </LevelItem>
-                                        <LevelItem className='ins-c-action__update-date'>
-                                            Updated on: { remediation.updated_at }
-                                        </LevelItem>
-                                    </Level>
-                                </CardHeader>
-                                <CardBody>
-                                    <Progress
-                                        value={ 19 }
-                                        label='16 of 62'
-                                        measureLocation={ ProgressMeasureLocation.outside } />
-                                </CardBody>
-                            </Card>
-                        </GridItem>
-                        <GridItem>
-                            <Card className='ins-c-card__system-reboot'>
-                                <CardHeader> Systems Reboot </CardHeader>
-                                <CardBody>
-                                    <Grid gutter="md" md={ 4 } sm={ 4 }>
-                                        <GridItem>
+                    <Stack gutter="md">
+                        <StackItem>
+                            <Grid gutter="md" sm={ 12 } md={ 4 } className='ins-c-summary-cards'>
+                                <GridItem>
+                                    <Card className='ins-c-card__actions-resolved'>
+                                        <CardHeader>
+                                            <Level>
+                                                <LevelItem>
+                                                    Actions Resolved
+                                                </LevelItem>
+                                                <LevelItem className='ins-c-action__update-date'>
+                                                    Updated on: { remediation.updated_at }
+                                                </LevelItem>
+                                            </Level>
+                                        </CardHeader>
+                                        <CardBody>
+                                            <Progress
+                                                value={ 19 }
+                                                label='16 of 62'
+                                                measureLocation={ ProgressMeasureLocation.outside } />
+                                        </CardBody>
+                                    </Card>
+                                </GridItem>
+                                <GridItem>
+                                    <Card className='ins-c-card__system-reboot'>
+                                        <CardHeader> Systems Reboot </CardHeader>
+                                        <CardBody>
+                                            <Grid gutter="md" md={ 4 } sm={ 4 }>
+                                                <GridItem>
+                                                    <Stack>
+                                                        <StackItem className='ins-m-text-emphesis'>11</StackItem>
+                                                        <StackItem>No reboot</StackItem>
+                                                    </Stack>
+                                                </GridItem>
+                                                <GridItem>
+                                                    <Stack>
+                                                        <StackItem className='ins-m-text-emphesis'>2</StackItem>
+                                                        <StackItem>Reboot Required</StackItem>
+                                                    </Stack>
+                                                </GridItem>
+                                                <GridItem>
+                                                    <Stack>
+                                                        <StackItem>
+                                                            <Switch
+                                                                id="autoReboot"
+                                                                aria-label="Auto Reboot"
+                                                                isChecked={ remediation.needs_reboot ? remediation.auto_reboot : false }
+                                                                isDisabled={ !remediation.needs_reboot }
+                                                                onChange={ this.handleRebootChange }
+                                                            />
+                                                        </StackItem>
+                                                        <StackItem>Auto Reboot</StackItem>
+                                                    </Stack>
+                                                </GridItem>
+                                            </Grid>
+                                        </CardBody>
+                                    </Card>
+                                </GridItem>
+                                <GridItem>
+                                    <Card className='ins-c-card__plan-details'>
+                                        <CardHeader> Plan Details </CardHeader>
+                                        <CardBody>
                                             <Stack>
-                                                <StackItem className='ins-m-text-emphesis'>11</StackItem>
-                                                <StackItem>No reboot</StackItem>
+                                                <StackItem>Created By: { remediation.created_by }</StackItem>
+                                                <StackItem>Date: { moment(remediation.created_at).format('ll') }</StackItem>
+                                                <StackItem> Shared with: foo</StackItem>
                                             </Stack>
-                                        </GridItem>
-                                        <GridItem>
-                                            <Stack>
-                                                <StackItem className='ins-m-text-emphesis'>2</StackItem>
-                                                <StackItem>Reboot Required</StackItem>
-                                            </Stack>
-                                        </GridItem>
-                                        <GridItem>
-                                            <Stack>
-                                                <StackItem>
-                                                    <Switch
-                                                        id="autoReboot"
-                                                        aria-label="Auto Reboot"
-                                                        isChecked={ remediation.needs_reboot ? remediation.auto_reboot : false }
-                                                        isDisabled={ !remediation.needs_reboot }
-                                                        onChange={ this.handleRebootChange }
-                                                    />
-                                                </StackItem>
-                                                <StackItem>Auto Reboot</StackItem>
-                                            </Stack>
-                                        </GridItem>
-                                    </Grid>
-                                </CardBody>
-                            </Card>
-                        </GridItem>
-                        <GridItem>
-                            <Card className='ins-c-card__plan-details'>
-                                <CardHeader> Plan Details </CardHeader>
-                                <CardBody>
-                                    <Stack>
-                                        <StackItem>Created By: { remediation.created_by }</StackItem>
-                                        <StackItem>Date: { moment(remediation.created_at).format('ll') }</StackItem>
-                                        <StackItem> Shared with: foo</StackItem>
-                                    </Stack>
-                                </CardBody>
-                            </Card>
-                        </GridItem>
-                    </Grid>
+                                        </CardBody>
+                                    </Card>
+                                </GridItem>
+                            </Grid>
+                        </StackItem>
+                        <StackItem>
+                            <RemediationDetailsTable remediation={ remediation }/>
+                        </StackItem>
+                    </Stack>
                 </Main>
             </React.Fragment>
         );
