e7c9058d4c5b4accbbc30d758b0b8b1c80da4347
Jozef Hartinger <jharting@redhat.com>
2019-01-30 12:05:16 +0100

    refactor api clients

---
+++ src/Utilities/http.js
@@ -0,0 +1,86 @@
+import get from 'lodash/get';
+
+const HEADERS = {
+    'Content-Type': 'application/json; charset=utf-8'
+};
+
+class HttpError extends Error {
+    constructor(description) {
+        super('Error communicating with the server');
+        this.description = description;
+    }
+}
+
+async function checkResponse (r) {
+    if (r.ok) {
+        return r;
+    }
+
+    if (r.status === 401) {
+        window.insights.chrome.auth.logout();
+        return;
+    }
+
+    if (r.headers.get('content-type').includes('application/json')) {
+        // let's try to extract some more info
+        let data = false;
+        try {
+            data = await r.json();
+        } catch (e) {} // eslint-disable-line no-empty
+
+        const error = get(data, 'errors[0]');
+
+        if (error) {
+            if (get(error, 'details.name')) {
+                throw new HttpError(`${error.title} (${error.details.name})`);
+            }
+
+            throw new HttpError(error.title);
+        }
+    }
+
+    throw new HttpError(`Unexpected response code ${r.status}`);
+}
+
+async function json (r) {
+    const type = r.headers.get('content-type');
+    if (!type.includes('application/json')) {
+        throw new HttpError(`Unexpected response type (${type}) returned`);
+    }
+
+    return r.json();
+}
+
+function doFetch (uri, method = 'GET', data = false, headers = false, options = {}) {
+    const opts = {
+        credentials: 'same-origin',
+        method,
+        ...options
+    };
+
+    if (headers) {
+        opts.headers = headers;
+    }
+
+    if (data) {
+        opts.body = JSON.stringify(data);
+    }
+
+    return fetch(uri, opts);
+}
+
+export function doGet (uri) {
+    return doFetch(uri.toString()).then(checkResponse).then(json);
+}
+
+export function doPost (uri, data) {
+    return doFetch(uri, 'POST', data, HEADERS).then(checkResponse).then(json);
+}
+
+export function doPatch (uri, data) {
+    return doFetch(uri, 'PATCH', data, HEADERS).then(checkResponse);
+}
+
+export function doDelete (uri) {
+    return doFetch(uri, 'DELETE').then(checkResponse);
+}
--- src/api.js
+++ src/api.js
@@ -1,128 +1,29 @@
 import { API_BASE } from './config';
-import get from 'lodash/get';
 
 import urijs from 'urijs';
+import { doGet, doPost, doPatch, doDelete } from './Utilities/http';
 
-class ApiError extends Error {
-    constructor(description) {
-        super('Error communicating with the server');
-        this.description = description;
-    }
+function url (...args) {
+    const url = urijs(API_BASE).segment('remediations');
+    args.forEach(segment => url.segment(segment));
+    return url;
 }
 
-async function checkResponse (r) {
-    if (r.ok) {
-        return r;
-    }
-
-    if (r.status === 401) {
-        window.insights.chrome.auth.logout();
-    }
-
-    if (r.headers.get('content-type').includes('application/json')) {
-        // let's try to extract some more info
-        const data = await r.json();
-
-        const error = get(data, 'errors[0]');
-
-        if (error) {
-            if (get(error, 'details.name')) {
-                throw new ApiError(`${error.title} (${error.details.name})`);
-            }
-
-            throw new ApiError(error.title);
-        }
-    }
-
-    throw new ApiError(`Unexpected response code ${r.status}`);
-}
-
-async function json (r) {
-    await checkResponse(r);
-
-    const type = r.headers.get('content-type');
-    if (!type.includes('application/json')) {
-        throw new ApiError(`Unexpected response type (${type}) returned`);
-    }
-
-    return r.json();
-}
-
-function patch (uri, body) {
-    return fetch(uri, {
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        method: 'PATCH',
-        body: JSON.stringify(body)
-    });
-}
-
-export function getRemediations () {
-    const uri = urijs(API_BASE).segment('remediations').toString();
-
-    return fetch(uri).then(json);
-}
-
-export function getRemediation (id) {
-    const uri = urijs(API_BASE).segment('remediations').segment(id).toString();
-    return fetch(uri).then(json);
-}
+export const getRemediations = () => doGet(url());
+export const getRemediation = id => doGet(url(id));
+export const createRemediation = data => doPost(url(), data);
+export const patchRemediation = (id, data) => doPatch(url(id), data).then(() => data);
+export const patchRemediationIssue = (id, issue, resolution) => doPatch(url(id, 'issues', issue), { resolution });
+export const deleteRemediation = id => doDelete(url(id));
+export const deleteRemediationIssue = (remediation, issue) => doDelete(url(remediation, 'issues', issue));
+export const getResolutions = issue => doGet(urijs(API_BASE).segment('resolutions').segment(issue));
 
 export function downloadPlaybook (id) {
-    const uri = urijs(API_BASE).segment('remediations').segment(id).segment('playbook').toString();
+    const uri = url(id, 'playbook').toString();
     window.open(uri);
-}
-
-export function createRemediation (data) {
-    const uri = urijs(API_BASE).segment('remediations').toString();
-
-    return fetch(uri, {
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8'
-        },
-        method: 'POST',
-        body: JSON.stringify(data)
-    }).then(json);
-}
-
-export function patchRemediation (id, data) {
-    const uri = urijs(API_BASE).segment('remediations').segment(id).toString();
-
-    return patch(uri, data)
-    .then(checkResponse)
-    .then(() => data);
-}
-
-export function patchRemediationIssue (id, issue, resolution) {
-    const uri = urijs(API_BASE).segment('remediations').segment(id).segment('issues').segment(issue).toString();
-
-    return patch(uri, { resolution })
-    .then(checkResponse);
 }
 
 // this is here for demo purposes only
 export function getHosts () {
-    return fetch('/r/insights/platform/inventory/api/v1/hosts').then(json);
-}
-
-export function deleteRemediation (id) {
-    const uri = urijs(API_BASE).segment('remediations').segment(id).toString();
-
-    return fetch(uri, {
-        method: 'DELETE'
-    }).then(checkResponse);
-}
-
-export function deleteRemediationIssue (remediation, issue) {
-    const uri = urijs(API_BASE).segment('remediations').segment(remediation).segment('issues').segment(issue).toString();
-
-    return fetch(uri, {
-        method: 'DELETE'
-    }).then(checkResponse);
-}
-
-export function getResolutions (issue) {
-    const uri = urijs(API_BASE).segment('resolutions').segment(issue).toString();
-    return fetch(uri).then(json);
+    return doGet('/r/insights/platform/inventory/api/v1/hosts');
 }
