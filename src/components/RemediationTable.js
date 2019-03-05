7036cd3258443b3c7b11938cbe6225412fe34f33
Jozef Hartinger <jharting@redhat.com>
2019-03-05 19:36:33 +0100

    RemediationTable - switch to hooks, add pagination

--- src/components/RemediationTable.js
+++ src/components/RemediationTable.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import debounce from 'lodash/debounce';
 import moment from 'moment';
 
 import { Link } from 'react-router-dom';
@@ -13,18 +12,17 @@ import {
     Split, SplitItem,
     Title, Button, TextInput
 } from '@patternfly/react-core';
-import { sortable, TableHeader, TableBody } from '@patternfly/react-table';
-import { SimpleTableFilter, TableToolbar } from '@red-hat-insights/insights-frontend-components';
+import { sortable, Table, TableHeader, TableBody } from '@patternfly/react-table';
+import { Pagination, SimpleTableFilter, TableToolbar } from '@red-hat-insights/insights-frontend-components';
 import { InfoCircleIcon } from '@patternfly/react-icons';
 
 import { formatUser, includesIgnoreCase } from '../Utilities/model';
 import { appUrl } from '../Utilities/urls';
 import './RemediationTable.scss';
 
-import SelectableTable from '../containers/SelectableTable';
 import SkeletonTable from './SkeletonTable';
 import { DeleteRemediationsButton } from '../containers/DeleteButtons';
-import { SEARCH_DEBOUNCE_DELAY } from '../constants';
+import { useFilter, usePagination, useSelector, useSorter } from '../hooks/table';
 
 function buildName (name, id) {
     return ({
@@ -36,148 +34,163 @@ function formatDate (date) {
     return moment(date).format('lll');
 }
 
+function skeleton () {
+    return (
+        <React.Fragment>
+            <TableToolbar className='ins-c-remediations-details-table__toolbar'>
+                <Level>
+                    <LevelItem>
+                        <TextInput
+                            type="text"
+                            value='Search Playbooks'
+                            aria-label="Search Playbooks Loading"
+                            isDisabled
+                        />
+                    </LevelItem>
+                    <LevelItem>
+                        <Split gutter="md">
+                            <SplitItem><Button isDisabled> Delete </Button></SplitItem>
+                        </Split>
+                    </LevelItem>
+                </Level>
+            </TableToolbar>
+            <SkeletonTable/>
+        </React.Fragment>
+    );
+}
+
+function empty () {
+    return (
+        <Card>
+            <CardBody>
+                <Bullseye>
+                    <EmptyState className='ins-c-no-remediations'>
+                        <EmptyStateIcon icon={ InfoCircleIcon } size='lg' />
+                        <Title size="lg">No Remediations</Title>
+                        <EmptyStateBody>
+                            <p>You haven&#39;t created any remediations yet.</p>
+                            <p>
+                                To create a remediation, please visit&nbsp;
+                                <a href={ appUrl('advisor') }>Insights</a>,&nbsp;
+                                <a href={ appUrl('vulnerability') }>Vulnerability</a> or&nbsp;
+                                <a href={ appUrl('compliance') }>Compliance</a>&nbsp;
+                                applications and look for the&nbsp;
+                                <strong>Remediate with Ansible</strong>
+                                &nbsp;button.
+                            </p>
+                        </EmptyStateBody>
+                    </EmptyState>
+                </Bullseye>
+            </CardBody>
+        </Card>
+    );
+}
+
 const SORTING_ITERATEES = [ null, 'name', 'system_count', 'issue_count', null, 'updated_at' ];
 
-class RemediationTable extends React.Component {
-    state = {
-        filter: '',
-        selected: [],
-        sortBy: 5,
-        sortDir: 'desc'
+function RemediationTable (props) {
+    const { value, status } = props;
+
+    const sorter = useSorter(5, 'desc');
+    const filter = useFilter();
+    const selector = useSelector();
+    const pagination = usePagination();
+
+    // Skeleton Loading
+    if (status !== 'fulfilled') {
+        return skeleton();
     }
 
-    onFilterChange = debounce(filter => this.setState({ filter }), SEARCH_DEBOUNCE_DELAY);
+    if (!value.remediations.length) {
+        return empty();
+    }
 
-    onSelect = selected => this.setState({ selected });
-
-    onSort = async (event, sortBy, sortDir) => {
+    filter.onChange(pagination.reset);
+    sorter.onChange((sortBy, sortDir) => {
         const column = SORTING_ITERATEES[sortBy];
-        await this.props.loadRemediations(column, sortDir);
-        this.setState({ sortBy, sortDir });
-    }
+        props.loadRemediations(column, sortDir);
+        pagination.reset();
+    });
 
-    render () {
-        const { value, status } = this.props;
-        const { filter, selected, sortBy, sortDir } = this.state;
+    const filtered = value.remediations.filter(r => includesIgnoreCase(r.name, filter.value.trim()));
+    const paged = filtered.slice(pagination.offset, pagination.offset + pagination.pageSize);
 
-        // Skeleton Loading
-        if (status !== 'fulfilled') {
-            return (
-                <React.Fragment>
-                    <TableToolbar className='ins-c-remediations-details-table__toolbar'>
-                        <Level>
-                            <LevelItem>
-                                <TextInput
-                                    type="text"
-                                    value='Search Playbooks'
-                                    aria-label="Search Playbooks Loading"
-                                    isDisabled
+    const rows = paged.map(remediation => ({
+        id: remediation.id,
+        cells: [
+            buildName(remediation.name, remediation.id),
+            remediation.system_count,
+            remediation.issue_count,
+            formatUser(remediation.updated_by),
+            formatDate(remediation.updated_at)
+        ]
+    }));
+
+    selector.register(rows);
+
+    const remediationIds = value.remediations.map(remediation => remediation.id);
+
+    return (
+        <React.Fragment>
+            <TableToolbar results={ filtered.length }>
+                <Level>
+                    <LevelItem>
+                        <SimpleTableFilter buttonTitle="" placeholder="Search Playbooks" { ...filter.props } />
+                    </LevelItem>
+                    <LevelItem>
+                        <Split gutter="md">
+                            <SplitItem>
+                                <DeleteRemediationsButton
+                                    isDisabled={ !selector.getSelectedIds(remediationIds).length }
+                                    remediations={ selector.getSelectedIds(remediationIds) }
                                 />
-                            </LevelItem>
-                            <LevelItem>
-                                <Split gutter="md">
-                                    <SplitItem><Button isDisabled> Delete </Button></SplitItem>
-                                </Split>
-                            </LevelItem>
-                        </Level>
-                    </TableToolbar>
-                    <SkeletonTable/>
-                </React.Fragment>
-            );
-        }
-
-        if (status === 'fulfilled' && !value.remediations.length) {
-            return (
-                <Card>
-                    <CardBody>
-                        <Bullseye>
-                            <EmptyState className='ins-c-no-remediations'>
-                                <EmptyStateIcon icon={ InfoCircleIcon } size='lg' />
-                                <Title size="lg">No Remediations</Title>
-                                <EmptyStateBody>
-                                    <p>You haven&#39;t created any remediations yet.</p>
-                                    <p>
-                                        To create a remediation, please visit&nbsp;
-                                        <a href={ appUrl('advisor') }>Insights</a>,&nbsp;
-                                        <a href={ appUrl('vulnerability') }>Vulnerability</a> or&nbsp;
-                                        <a href={ appUrl('compliance') }>Compliance</a>&nbsp;
-                                        applications and look for the&nbsp;
-                                        <strong>Remediate with Ansible</strong>
-                                        &nbsp;button.
-                                    </p>
-                                </EmptyStateBody>
-                            </EmptyState>
-                        </Bullseye>
-                    </CardBody>
-                </Card>
-            );
-        }
-
-        const filtered = value.remediations.filter(r => includesIgnoreCase(r.name, filter.trim()));
-
-        const rows = filtered.map(remediation => ({
-            id: remediation.id,
-            cells: [
-                buildName(remediation.name, remediation.id),
-                remediation.system_count,
-                remediation.issue_count,
-                formatUser(remediation.updated_by),
-                formatDate(remediation.updated_at)
-            ]
-        }));
-
-        return (
-            <React.Fragment>
-                <TableToolbar results={ value.remediations.length }>
-                    <Level>
-                        <LevelItem>
-                            <SimpleTableFilter buttonTitle="" placeholder="Search Playbooks" onFilterChange={ this.onFilterChange } />
-                        </LevelItem>
-                        <LevelItem>
-                            <Split gutter="md">
-                                <SplitItem>
-                                    <DeleteRemediationsButton
-                                        isDisabled={ !selected.length }
-                                        remediations={ selected }
-                                    />
-                                </SplitItem>
-                            </Split>
-                        </LevelItem>
-                    </Level>
+                            </SplitItem>
+                        </Split>
+                    </LevelItem>
+                </Level>
+            </TableToolbar>
+            {
+                rows.length > 0 ?
+                    <Table
+                        aria-label="Playbooks"
+                        cells={ [
+                            {
+                                title: 'Playbook',
+                                transforms: [ sortable ]
+                            }, {
+                                title: 'Systems',
+                                transforms: [ sortable ]
+                            }, {
+                                title: 'Actions',
+                                transforms: [ sortable ]
+                            }, {
+                                title: 'Last Modified By'
+                            }, {
+                                title: 'Last Modified On',
+                                transforms: [ sortable ]
+                            }]
+                        }
+                        rows={ rows }
+                        { ...sorter.props }
+                        { ...selector.props }
+                    >
+                        <TableHeader/>
+                        <TableBody/>
+                    </Table> :
+                    <p className='ins-c-remediations-table--empty'>No Playbooks found</p>
+            }
+            {
+                rows.length > 0 &&
+                <TableToolbar>
+                    <Pagination
+                        numberOfItems={ filtered.length }
+                        useNext={ true }
+                        { ...pagination.props }
+                    />
                 </TableToolbar>
-                {
-                    filtered.length ?
-                        <SelectableTable
-                            aria-label="Playbooks"
-                            cells={ [
-                                {
-                                    title: 'Playbook',
-                                    transforms: [ sortable ]
-                                }, {
-                                    title: 'Systems',
-                                    transforms: [ sortable ]
-                                }, {
-                                    title: 'Actions',
-                                    transforms: [ sortable ]
-                                }, {
-                                    title: 'Last Modified By'
-                                }, {
-                                    title: 'Last Modified On',
-                                    transforms: [ sortable ]
-                                }]
-                            }
-                            onSelect={ this.onSelect }
-                            onSort={ this.onSort }
-                            sortBy={ { index: sortBy, direction: sortDir } }
-                            rows={ rows }>
-                            <TableHeader/>
-                            <TableBody/>
-                        </SelectableTable> :
-                        <p className='ins-c-remediations-table--empty'>No Playbooks found</p>
-                }
-            </React.Fragment>
-        );
-    }
+            }
+        </React.Fragment>
+    );
 }
 
 RemediationTable.propTypes = {
