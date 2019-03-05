ddff2d497f2eb49baf68591e08ff2b7fde953299
Jozef Hartinger <jharting@redhat.com>
2019-03-05 20:02:44 +0100

    add debug utility for pagination

---
+++ src/Utilities/debug.js
@@ -0,0 +1,6 @@
+const debug = localStorage.getItem('remediations:debug') === 'true';
+
+export const pagination = debug ?
+    {
+        perPageOptions: [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 50, 100, 200 ]
+    } : {};
