9f81f2a684bf5d9f2cd89c2de97557a8ea72b3bd
Jozef Hartinger <jharting@redhat.com>
2019-03-20 10:44:15 +0100

    update spandx configs

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -5,8 +5,5 @@ const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docke
 exports.routes = {
     '/rhcs/remediations': { host: `http://${localhost}:8002` },
     '/insights/remediations': { host: `http://${localhost}:8002` },
-    '/apps/remediations': { host: `http://${localhost}:8002` },
-
-    '/api/remediations': { host: `http://${localhost}:9002` },
-    '/r/insights/platform/remediations': { host: `http://${localhost}:9002` }
+    '/apps/remediations': { host: `http://${localhost}:8002` }
 };
