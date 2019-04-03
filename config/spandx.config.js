b639d1aa78659188a2fcbf2f81fada52e77ec6ad
Jozef Hartinger <jharting@redhat.com>
2019-04-03 08:09:05 +0200

    modify spandx config to include /beta; introduce BUILD_BETA

--- config/spandx.config.js
+++ config/spandx.config.js
@@ -5,5 +5,8 @@ const localhost = (process.env.PLATFORM === 'linux') ? 'localhost' : 'host.docke
 exports.routes = {
     '/rhcs/remediations': { host: `http://${localhost}:8002` },
     '/insights/remediations': { host: `http://${localhost}:8002` },
-    '/apps/remediations': { host: `http://${localhost}:8002` }
+    '/apps/remediations': { host: `http://${localhost}:8002` },
+    '/beta/rhcs/remediations': { host: `http://${localhost}:8002` },
+    '/beta/insights/remediations': { host: `http://${localhost}:8002` },
+    '/beta/apps/remediations': { host: `http://${localhost}:8002` }
 };
