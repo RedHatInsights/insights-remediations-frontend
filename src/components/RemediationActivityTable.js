bfa3db70d7b265671b6fa115509bac5422fbdc5c
Ryan Long <rlong@redhat.com>
2020-03-26 17:00:25 -0400

    add activity tab and empty states

--- src/components/RemediationActivityTable.js
+++ src/components/RemediationActivityTable.js
@@ -1,143 +1,91 @@
-import React, { useState, useContext } from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import PropTypes from 'prop-types';
 
-import flatMap from 'lodash/flatMap';
-import orderBy from 'lodash/orderBy';
+import { Button, EmptyState, EmptyStateBody, EmptyStateIcon, Title, Bullseye } from '@patternfly/react-core';
+import { TableHeader, Table, TableBody, expandable } from '@patternfly/react-table';
+import SkeletonTable from '../skeletons/SkeletonTable';
 
-import { Button } from '@patternfly/react-core';
-import { TableHeader, Table, TableBody } from '@patternfly/react-table';
-import { CheckCircleIcon, TimesCircleIcon, InProgressIcon } from '@patternfly/react-icons';
+import { statusSummary, normalizeStatus } from './statusHelper';
 
-import { EmptyTable } from '@redhat-cloud-services/frontend-components';
-
-import { getIssueApplication, includesIgnoreCase } from '../Utilities/model';
-import './RemediationTable.scss';
-
-import { useFilter, usePagination, useSelector, useSorter, useExpander } from '../hooks/table';
+import { getPlaybookRuns, loadRemediation } from '../actions';
 
 import './RemediationActivityTable.scss';
-import { PermissionContext } from '../App';
 
-function getRunOnDate() {
-    const url = <a href='#'>test</a>
-    return url
-}
+import { WrenchIcon } from '@patternfly/react-icons';
 
-function getRunByPerson() {
-    return 'me'
-}
+const RemediationActivityTable = ({
+    remediation,
+    playbookRuns,
+    getPlaybookRuns
+}) => {
 
-function getStatus(status) {
-    const permission = useContext(PermissionContext);
-    const statusString = status !== 'in_progress' ? status : 'In progress';
+    useEffect(() => {
+        getPlaybookRuns(remediation.id);
+    }, [ getPlaybookRuns ]);
+
+    const systemsStatus = { running: 1, success: 2, failure: 1 };
+
+    // TOOD, make expandable, fill in that data
+    if (remediation && playbookRuns) {
+        return playbookRuns.length ? (
+            <Table
+                aria-label="Activity Table"
+                rows={ playbookRuns.map(playbooks => (
+                    {
+                        isOpen: true,
+                        cells: [
+                            { title: <Link to={ `/${remediation.id}/${playbooks.id}` }> { playbooks.created_at } </Link>,
+                                cellFormatters: [ expandable ]},
+                            `${playbooks.created_by.first_name} ${playbooks.created_by.last_name}`,
+                            statusSummary(normalizeStatus(playbooks.status), systemsStatus)
+                        ]
+                    }
+                )) }
+                cells={ [{ title: 'Run on' }, { title: 'Run by' }, { title: 'Status' }] }>
+                <TableHeader />
+                <TableBody />
+            </Table>
+        ) : (
+            <Bullseye>
+                <EmptyState>
+                    <EmptyStateIcon icon={ WrenchIcon } />
+                    <Title headingLevel="h5" size="lg">
+                        Do more with Find it Fix it.
+                    </Title>
+                    <EmptyStateBody>
+                        Configure Cloud Connector to connect cloud.redhat.com with your
+                        Satellite instances and execute remediation across all regions,
+                        geographies, and Satellites in one place.
+                    </EmptyStateBody>
+                    <Button variant="link">Learn how to configure</Button>
+                </EmptyState>
+            </Bullseye>
+        );
+    }
 
     return (
-        <React.Fragment>
-            <b className={`ins-activity-status__text ins-activity-status__text-${status}`}>{statusString}</b>
-            <span className='ins-activity-status__icon ins-activity-status__icon-passed'>
-                <CheckCircleIcon/> 10
-            </span>
-            <span className='ins-activity-status__icon ins-activity-status__icon-failed'>
-                <TimesCircleIcon/> 20
-            </span>
-            <span className='ins-activity-status__icon ins-activity-status__icon-in_progress'>
-                <InProgressIcon/> 0
-            </span>
-            { status === 'in_progress' && permission.permissions.execute &&
-                <Button variant='link'> Cancel process</Button>
-            }
-        </React.Fragment>
+        <SkeletonTable/>
     );
-}
 
-const SORTING_ITERATEES = [
-    null, // expand toggle
-    null, // checkboxes
-    i => i.description,
-    null, // resolution steps
-    i => i.resolution.needs_reboot,
-    i => i.systems.length,
-    i => getIssueApplication(i)
-];
-
-const buildRow = (remediation, expanded) => (issue, index) => {
-    const row = [
-        {
-            isOpen: false,
-            id: issue.id,
-            cells: [
-                {
-                    title: getRunOnDate()
-                },
-                {
-                    title: getRunByPerson()
-                },
-                {
-                    title: getStatus('in_progress')
-                }
-            ]
-        },
-        {
-            parent: index * 2,
-            fullWidth: true,
-            cells: ['child - 1']
-        },
-    ];
-
-    return row;
 };
-
-function RemediationActivityTable (props) {
-    const pagination = usePagination();
-    const sorter = useSorter(2, 'asc');
-    const filter = useFilter();
-    const selector = useSelector();
-    const expander = useExpander();
-
-    sorter.onChange(pagination.reset);
-    filter.onChange(pagination.reset);
-
-    const filtered = props.remediation.issues.filter(i => includesIgnoreCase(i.description, filter.value.trim()));
-    const sorted = orderBy(filtered, [ SORTING_ITERATEES[ sorter.sortBy] ], [ sorter.sortDir ]);
-    const paged = sorted.slice(pagination.offset, pagination.offset + pagination.pageSize);
-
-    const rows = flatMap(paged, buildRow(props.remediation, expander.value));
-
-    selector.register(rows);
-    expander.register(rows);
-
-    return (
-        <React.Fragment>
-            {
-                rows.length > 0 ?
-                    <Table
-                        aria-label="Actions"
-                        className='ins-c-remediations-activity-table'
-                        cells={ [
-                            {
-                                title: 'Run on'
-                            }, {
-                                title: 'Run by'
-                            }, {
-                                title: 'Status'
-                            }]
-                        }
-                        rows={ rows }
-                        { ...expander.props }
-                    >
-                        <TableHeader />
-                        <TableBody { ...selector.tbodyProps } />
-                    </Table> :
-                    <EmptyTable centered className='ins-c-remediation-details-table--empty'>This Playbook is empty</EmptyTable>
-            }
-        </React.Fragment>
-
-    );
-}
 
 RemediationActivityTable.propTypes = {
-    remediation: PropTypes.object.isRequired,
-    status: PropTypes.object.isRequired
+    remediation: PropTypes.object,
+    playbookRuns: PropTypes.object,
+    getPlaybookRuns: PropTypes.func
 };
 
-export default RemediationActivityTable;
+const connected = connect(
+    ({ playbookRuns, selectedRemediation }) => ({
+        playbookRuns: playbookRuns.data,
+        remediation: selectedRemediation.remediation
+    }),
+    (dispatch) => ({
+        getPlaybookRuns: (id) => dispatch(getPlaybookRuns(id)),
+        loadRemediation: id => dispatch(loadRemediation(id))
+    })
+)(RemediationActivityTable);
+export default connected;
