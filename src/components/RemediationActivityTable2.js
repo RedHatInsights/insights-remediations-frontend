ada7de4a231189bcc7648ebf6cd5aac9d8e0ba4b
Ryan Long <rlong@redhat.com>
2020-03-31 11:29:44 -0400

    working

--- src/components/RemediationActivityTable2.js
+++ src/components/RemediationActivityTable2.js
@@ -15,7 +15,48 @@ const RemediationActivityTable = ({ remediation, playbookRuns }) => {
 
     const [rows, setRows] = useState([]);
 
+    useEffect(() => {
+        if (playbookRuns && playbookRuns.length) {
+            setRows(generateRows);
+        }
+    },[playbookRuns]);
+    
     const systemsStatus = { running: 1, success: 2, failure: 1 };
+
+    const generateRows = () => {
+        return (playbookRuns.reduce((acc, playbooks, i) => (
+            [
+                {
+                    isOpen: false,
+                    cells: [
+                        { title: <Link to={ `/${remediation.id}/${playbooks.id}` }> { playbooks.created_at } </Link>,
+                            cellFormatters: [ expandable ]},
+                        `${playbooks.created_by.first_name} ${playbooks.created_by.last_name}`,
+                        { title: statusSummary(normalizeStatus(playbooks.status), systemsStatus) }
+                    ]
+                }, {
+                    parent: 2 * i,
+                    fullWidth: true,
+                    cells: [{
+                        title: <Table
+                            aria-label="Compact expandable table"
+                            cells={ [ 'Connection', 'Systems', 'Playbook runs status' ] }
+                            rows={ playbooks.executors.map(e => (
+                                { cells: [
+                                    { title: <Link to={ `/${remediation.id}/${playbooks.id}/${e.executor_id}` }>{ e.executor_name }</Link> },
+                                    e.system_count,
+                                    { title: statusSummary(normalizeStatus(playbooks.status), systemsStatus)}
+                                ]}
+                            )) }
+                        >
+                            <TableHeader />
+                            <TableBody />
+                        </Table>
+                    }]
+                }
+            ]
+        ), []))
+    }
 
     const handleOnCollapse = (event, rowId, isOpen) => {
         const collapseRows = [...rows];
@@ -28,47 +69,6 @@ const RemediationActivityTable = ({ remediation, playbookRuns }) => {
         'Run by',
         'Status'
     ];
-
-    const generateRows = () => {
-        if (playbookRuns && playbookRuns.length) {
-            return (playbookRuns.reduce((acc, playbooks, i) => (
-                [
-                    {
-                        isOpen: false,
-                        cells: [
-                            { title: <Link to={ `/${remediation.id}/${playbooks.id}` }> { playbooks.created_at } </Link>,
-                                cellFormatters: [ expandable ]},
-                            `${playbooks.created_by.first_name} ${playbooks.created_by.last_name}`,
-                            { title: statusSummary(normalizeStatus(playbooks.status), systemsStatus) }
-                        ]
-                    }, {
-                        parent: 2 * i,
-                        fullWidth: true,
-                        cells: [{
-                            title: <Table
-                                aria-label="Compact expandable table"
-                                cells={ [ 'Connection', 'Systems', 'Playbook runs status' ] }
-                                rows={ playbooks.executors.map(e => (
-                                    { cells: [
-                                        { title: <Link to={ `/${remediation.id}/${playbooks.id}/${e.executor_id}` }>{ e.executor_name }</Link> },
-                                        e.system_count,
-                                        { title: statusSummary(normalizeStatus(playbooks.status), systemsStatus)}
-                                    ]}
-                                )) }
-                            >
-                                <TableHeader />
-                                <TableBody />
-                            </Table>
-                        }]
-                    }
-                ]
-            ), [playbookRuns]))
-        }
-    }
-
-    useEffect(() => {
-        setRows(generateRows);
-    },[]);
     
     return (
         <Table aria-label="Collapsible table" onCollapse={ handleOnCollapse } rows={rows} cells={columns}>
