a919dcd5dcd1f627243f6de78da29c0f8d5c48f9
Ryan Long <rlong@redhat.com>
2020-11-05 16:01:21 -0500

    update to use 404 page

--- src/components/EmptyStates/NoRemediation.js
+++ src/components/EmptyStates/NoRemediation.js
@@ -1,24 +1,10 @@
 
 import React from 'react';
 
-import { Button, EmptyState, EmptyStateIcon, Title, Bullseye } from '@patternfly/react-core';
-import { WrenchIcon } from '@patternfly/react-icons';
+import { InvalidObject } from '@redhat-cloud-services/frontend-components';
 
 const NoRemediation = () => (
-    <Bullseye>
-        <EmptyState>
-            <EmptyStateIcon icon={ WrenchIcon } />
-            <Title headingLevel="h5" size="lg">
-                Remediation not found
-            </Title>
-            <Button
-                variant="link"
-                component="a"
-                href={ `./${window.insights.chrome.isBeta() ? 'beta/' : ''}insights/remediations` }>
-                    Back to Remediations
-            </Button>
-        </EmptyState>
-    </Bullseye>
+    <InvalidObject/>
 );
 
 export default NoRemediation;
