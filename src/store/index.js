a9b854d0e4afa12904717ca3d52542d41085f883
Martin Marosi <marvusm.mmi@gmail.com>
2020-12-08 14:13:23 +0100

    Add missing webpack loaders.

--- src/store/index.js
+++ src/store/index.js
@@ -19,7 +19,7 @@ export function init(...middleware) {
       errorTitleKey: 'message',
       errorDescriptionKey: 'description',
     }),
-    ...middleware,
+    ...middleware.filter((item) => typeof item !== 'undefined'),
   ]);
 
   registry.register(reducers);
