70b5e7be572b2a342c03505ced0c04cb743962db
Martin Maroši <marvusm.mmi@gmail.com>
2021-02-23 10:42:02 +0100

    Use remediations v3. (#219)

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -2,6 +2,7 @@ const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
+  https: false,
 });
 
 const modulesConfig = require('@redhat-cloud-services/frontend-components-config/federated-modules')(
