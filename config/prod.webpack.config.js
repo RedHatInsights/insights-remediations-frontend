81ecbce92636857f5c9bbbe80f35297c31b6b9ee
Adonis Puente <adonis.puente@gmail.com>
2023-12-12 11:58:11 -0500

    fix(wepback): update webpack configs to contain deployment tag

--- config/prod.webpack.config.js
+++ config/prod.webpack.config.js
@@ -3,6 +3,7 @@ const config = require('@redhat-cloud-services/frontend-components-config');
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   https: false,
+  ...(process.env.BETA === 'true' && { deployment: 'beta/apps' }),
 });
 
 plugins.push(
