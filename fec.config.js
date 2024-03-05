84ecd7c597dc28fea38f69db4fb5ffa6089cf601
Muslimjon <59481011+mkholjuraev@users.noreply.github.com>
2024-03-05 14:17:50 +0100

    Update hot reload config in fec.config.js
    Co-authored-by: Sebastian Gräßl <bastilian@users.noreply.github.com>

--- fec.config.js
+++ fec.config.js
@@ -7,7 +7,7 @@ module.exports = {
   proxyVerbose: true,
   interceptChromeConfig: false,
   plugins: [],
-  hotReload: true,
+  ...(process.env.HOT ? { hotReload: process.env.HOT === 'true' } : { hotReload: true }),
   ...(process.env.port ? { port: parseInt(process.env.port) } : {}),
   moduleFederation: {
     exclude: ['react-router-dom'],
