aeb3e248c08631c954ef595ae03663c2acb00f99
Adonis Puente <adonis.puente@gmail.com>
2024-04-10 12:13:13 -0400

    update test for react18

--- src/routes/RemediationDetails.test.js
+++ src/routes/RemediationDetails.test.js
@@ -1,5 +1,6 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import { useConnectionStatus } from '../Utilities/useConnectionStatus';
-import { renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react';
 import { useAxiosWithPlatformInterceptors } from '@redhat-cloud-services/frontend-components-utilities/interceptors';
 
 jest.mock(
@@ -20,12 +21,13 @@ describe('useConnectionStatus', () => {
         return res;
       },
     }));
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useConnectionStatus(remediation)
-    );
+    let hook;
+    await act(async () => {
+      hook = renderHook(() => useConnectionStatus(remediation));
+    });
+    const { result } = hook;
 
-    await waitForNextUpdate();
-    expect(result.current).toBe(true);
+    expect(result.current[0]).toBe(true);
   });
   test('fires off a request and returns FALSE', async () => {
     useAxiosWithPlatformInterceptors.mockImplementation(() => ({
@@ -34,11 +36,11 @@ describe('useConnectionStatus', () => {
         return res;
       },
     }));
-    const { result, waitForNextUpdate } = renderHook(() =>
-      useConnectionStatus(remediation)
-    );
-
-    await waitForNextUpdate();
-    expect(result.current).toBe(false);
+    let hook;
+    await act(async () => {
+      hook = renderHook(() => useConnectionStatus(remediation));
+    });
+    const { result } = hook;
+    expect(result.current[0]).toBe(false);
   });
 });
