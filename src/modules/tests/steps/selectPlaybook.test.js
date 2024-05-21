8f7795e18bc86f79761dfe96bda81528497d5510
Michael Johnson <johnsonm325@gmail.com>
2024-05-21 10:59:59 -0400

    fix(RHINENG-8088): refactor select playbook typeahead (#422)
    * fix(RHINENG-8088): refactor select playbook typeahead
    
    To repro:
    Choose and click a system in inventory
    From the system details page, click the Vulnerability tab
    Pick CVE(s) and click on Remediate button
    When modal opens - click in the "Select playbook" box
    
    This box should allow typeahead, which filters the existing playbooks as you type. But this feature seems to have been removed from the PF component we were using and have been moved to the Select component.
    
    This PR refactors this box to allow typeahead.
    
    * Add suggested fixes and more tests
    
    ---------
    
    Co-authored-by: Michael Johnson <micjohns@micjohns-thinkpadp16vgen1.rmtusnc.csb>

--- src/modules/tests/steps/selectPlaybook.test.js
+++ src/modules/tests/steps/selectPlaybook.test.js
@@ -12,13 +12,19 @@ import { Provider } from 'react-redux';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
+import { getRemediation } from '../../../api';
 
 jest.mock('../../../api', () => ({
   ...jest.requireActual('../../../api'),
   getRemediations: jest.fn(
     () =>
       new Promise((resolve) =>
-        resolve({ data: [{ id: 'remediationId', name: 'test-remediation-1' }] })
+        resolve({
+          data: [
+            { id: 'remediationId', name: 'test-remediation-1' },
+            { id: '1234', name: 'bretheren' },
+          ],
+        })
       )
   ),
   getRemediation: jest.fn(
@@ -117,8 +123,136 @@ describe('SelectPlaybook', () => {
     );
 
     await waitFor(() => {
-      expect(screen.getByText('test-remediation-1')).toBeVisible();
+      expect(screen.queryByTestId('skeleton-loader')).not.toBeInTheDocument();
     });
+
+    await waitFor(() => {
+      expect(
+        screen.getByRole('combobox', {
+          name: /type to filter/i,
+        })
+      ).toBeVisible();
+    });
+  });
+
+  it('should use type ahead on existing playbooks dropdown', async () => {
+    const store = mockStore(initialState);
+    render(
+      <Provider store={store}>
+        <RendererWrapper schema={createSchema({})} {...initialProps} />
+      </Provider>
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('skeleton-loader')).not.toBeInTheDocument();
+    });
+
+    const typeaheadBox = screen.getByRole('combobox', {
+      name: /type to filter/i,
+    });
+    await userEvent.click(typeaheadBox);
+
+    expect(
+      screen.getByRole('option', {
+        name: /test-remediation-1/i,
+      })
+    ).toBeVisible();
+
+    expect(
+      screen.getByRole('option', {
+        name: /bretheren/i,
+      })
+    ).toBeVisible();
+
+    await userEvent.type(typeaheadBox, 'br');
+
+    expect(
+      screen.queryByRole('option', {
+        name: /test-remediation-1/i,
+      })
+    ).not.toBeInTheDocument();
+
+    expect(
+      screen.getByRole('option', {
+        name: /bretheren/i,
+      })
+    ).toBeVisible();
+  });
+
+  it('should display no results found on typeahead', async () => {
+    const store = mockStore(initialState);
+    render(
+      <Provider store={store}>
+        <RendererWrapper schema={createSchema({})} {...initialProps} />
+      </Provider>
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('skeleton-loader')).not.toBeInTheDocument();
+    });
+
+    const typeaheadBox = screen.getByRole('combobox', {
+      name: /type to filter/i,
+    });
+    await userEvent.click(typeaheadBox);
+
+    expect(
+      screen.getByRole('option', {
+        name: /test-remediation-1/i,
+      })
+    ).toBeVisible();
+
+    expect(
+      screen.getByRole('option', {
+        name: /bretheren/i,
+      })
+    ).toBeVisible();
+
+    await userEvent.type(typeaheadBox, 'tooted');
+
+    expect(
+      screen.queryByRole('option', {
+        name: /test-remediation-1/i,
+      })
+    ).not.toBeInTheDocument();
+
+    expect(
+      screen.queryByRole('option', {
+        name: /bretheren/i,
+      })
+    ).not.toBeInTheDocument();
+
+    expect(
+      screen.getByRole('option', {
+        name: /no results found for "tooted"/i,
+      })
+    ).toBeVisible();
+  });
+
+  it('should call getRemediation on select', async () => {
+    const store = mockStore(initialState);
+    render(
+      <Provider store={store}>
+        <RendererWrapper schema={createSchema({})} {...initialProps} />
+      </Provider>
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('skeleton-loader')).not.toBeInTheDocument();
+    });
+
+    const typeaheadBox = screen.getByRole('combobox', {
+      name: /type to filter/i,
+    });
+    await userEvent.click(typeaheadBox);
+
+    await userEvent.click(
+      screen.queryByRole('option', {
+        name: /test-remediation-1/i,
+      })
+    );
+
+    expect(getRemediation).toHaveBeenCalled();
   });
 
   it('should be able to create new playbook', async () => {
