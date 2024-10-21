16700e41ccd6770b5264daeea1f8293fe3cfec71
Georgii Karataev <georgkarat@gmail.com>
2024-10-21 10:13:19 +0200

    chore(deps): Upgrade FEC dependencies, clean unused ones (#447)
    Fixes https://issues.redhat.com/browse/RHINENG-12005.

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,6 @@ module.exports = {
   plugins: [
     '@babel/plugin-transform-runtime',
     '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-class-properties',
-    'lodash',
   ],
   env: {
     componentTest: {
