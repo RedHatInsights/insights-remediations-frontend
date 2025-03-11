3a8dfaf80258f2548eb84a91c001421eed1a35ca
Adonis Puente <adonis.puente@gmail.com>
2025-03-11 16:45:45 -0500

    add feature flag

--- src/api/index.js
+++ src/api/index.js
@@ -179,7 +179,6 @@ export function patchRemediation(id, data) {
 }
 
 export function getRemediations() {
-  console.log('firing off here');
   const uri = new urijs(API_BASE)
     .segment('remediations')
     .query({ limit: 200 })
