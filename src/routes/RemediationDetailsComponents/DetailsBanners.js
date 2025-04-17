4e7b3e16e07be4152f3f888f3de449edf729b189
Adonis Puente <60629070+adonispuente@users.noreply.github.com>
2025-04-17 13:12:30 -0500

    feat(RHINENG-16099): Remediations Remodel Details Page : General Tab + Foundation (#492)
    * feat(DetailsPageGeneralPage): Foundation and General Tab Conent + Banner Foundation
    
    * Add Banner component: Need to figure out input source
    
    * Found input source for banner and row, moved helper function
    
    * clean up and refactor rename to work with V2
    
    * banner rearrangement, only danger banner for general tab
    
    * small UX changes
    
    * add dispatch flow for DetailsPageHeader

---
+++ src/routes/RemediationDetailsComponents/DetailsBanners.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Alert } from '@patternfly/react-core';
+
+//This will be for Execution History Tab
+const DetailsBanner = ({ status, remediationPlanName, canceledAt }) => {
+  let variant = 'info';
+  let title = '';
+  let description = '';
+
+  switch (status) {
+    case 'running':
+      variant = 'info';
+      title = 'The execution of the remediation plan is in progress.';
+      description =
+        'For more information you can review the log file for the individual systems that this remediation plan was executed on.';
+      break;
+    case 'succeded':
+      variant = 'success';
+      title = 'The execution of the remediation plan succeeded.';
+      description =
+        'Review the issue resolution status in the respective service (for example Advisor recommendations).';
+      break;
+    case 'failure':
+      variant = 'danger';
+      title = 'The execution of the remediation plan failed.';
+      description =
+        'For more information you can review the log file for the individual systems that this remediation plan was executed on.';
+      break;
+    case 'canceled':
+      variant = 'danger';
+      title = 'The execution of the remediation plan was canceled.';
+      description = `The execution of the remediation plan ${
+        remediationPlanName || '[RemediationPlanName]'
+      } was canceled on ${canceledAt}.`;
+      break;
+    default:
+      return null;
+  }
+
+  return (
+    <Alert
+      isInline
+      variant={variant}
+      title={title}
+      className="pf-v5-u-mb-md"
+      //TODO: UX has removed actions until after summit
+      // actionLinks={
+      //   <Flex>
+      //     <AlertActionLink component="a" href="#">
+      //       View details
+      //     </AlertActionLink>
+      //     <AlertActionLink onClick={() => console.log('Clicked on Ignore')}>
+      //       Ignore
+      //     </AlertActionLink>
+      //   </Flex>
+      // }
+    >
+      <p>{description}</p>
+    </Alert>
+  );
+};
+
+DetailsBanner.propTypes = {
+  status: PropTypes.oneOf(['running', 'succeeded', 'failed', 'canceled'])
+    .isRequired,
+  remediationPlanName: PropTypes.string,
+  canceledAt: PropTypes.string,
+};
+
+export default DetailsBanner;
---
+++ src/routes/RemediationDetailsComponents/helpers.js
@@ -0,0 +1,77 @@
+import { Flex, Icon, TextContent } from '@patternfly/react-core';
+import {
+  CheckCircleIcon,
+  ExclamationCircleIcon,
+  InProgressIcon,
+} from '@patternfly/react-icons';
+import React from 'react';
+
+export const execStatus = (status, date) => {
+  let icon;
+  let displayValue = 'N/A';
+
+  if (!date || !status) {
+    return (
+      <Flex spaceItems={{ default: 'spaceItemsSm' }}>
+        <TextContent>{displayValue}</TextContent>
+      </Flex>
+    );
+  }
+
+  if (status === 'success') {
+    (icon = (
+      <Icon status="success">
+        <CheckCircleIcon />
+      </Icon>
+    )),
+      (displayValue = 'Succeeded');
+  } else if (status === 'running') {
+    (icon = (
+      <Icon>
+        <InProgressIcon color="var(--pf-v5-global--icon--Color--light--dark)" />
+      </Icon>
+    )),
+      (displayValue = 'In progress');
+  } else if (status === 'failure') {
+    (icon = (
+      <Icon status="danger">
+        <ExclamationCircleIcon />
+      </Icon>
+    )),
+      (displayValue = 'Failed');
+  }
+  return (
+    <Flex spaceItems={{ default: 'spaceItemsSm' }}>
+      {icon}
+      <TextContent>{`${displayValue} ${getTimeAgo(date)}`}</TextContent>
+    </Flex>
+  );
+};
+
+export const getTimeAgo = (date) => {
+  const now = new Date();
+  const diffMs = now - date;
+  const diffMinutes = Math.floor(diffMs / 60000);
+
+  if (diffMinutes < 1) {
+    return 'Just now';
+  } else if (diffMinutes < 60) {
+    return `${diffMinutes} minute${diffMinutes === 1 ? '' : 's'} ago`;
+  }
+  //If less than 30 days, report days, if less than 12 months report months, then years
+  const diffHours = Math.floor(diffMinutes / 60);
+  if (diffHours < 24) {
+    return `${diffHours} hour${diffHours === 1 ? '' : 's'} ago`;
+  }
+
+  const diffDays = Math.floor(diffHours / 24);
+  if (diffDays < 30) {
+    return `${diffDays} day${diffDays === 1 ? '' : 's'} ago`;
+  }
+  const diffMonths = Math.floor(diffDays / 30);
+  if (diffMonths < 12) {
+    return `${diffMonths} month${diffMonths === 1 ? '' : 's'} ago`;
+  }
+  const diffYears = Math.floor(diffDays / 365);
+  return `${diffYears} year${diffYears === 1 ? '' : 's'} ago`;
+};
