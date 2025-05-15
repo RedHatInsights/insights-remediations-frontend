11e50a6525445be19f459ecabc1b8e5269e91245
Oliver Pacut <opacut@redhat.com>
2025-05-15 12:19:21 +0200

    cleanup OverViewPage

--- src/routes/OverViewPage/OverViewPage.cy.js
+++ src/routes/OverViewPage/OverViewPage.cy.js
@@ -1,24 +1,14 @@
 import React from 'react';
-//import RemediationsTable from './RemediationsTable';
 import { OverViewPage } from '../../routes/OverViewPage/OverViewPage';
-import { Home } from '../../routes/Home';
 import { DEFAULT_RENDER_OPTIONS } from '../../Frameworks/AsyncTableTools/AsyncTableTools/utils/testHelpers';
-
-const MountRemediationsTable = () => {
-  return <RemediationsTable />;
-};
 
 const MountOverviewPage = () => {
   return <OverViewPage />;
 };
 
-const MountHome = () => {
-  return <Home />;
-};
-
 describe('OverviewPage tests', () => {
   beforeEach(() => {
-    cy.mountWithContext(MountOverviewPage);
+    cy.mountWithContext(MountOverviewPage, DEFAULT_RENDER_OPTIONS);
   });
   it('renders the table correctly', () => {
     cy.get('body');
