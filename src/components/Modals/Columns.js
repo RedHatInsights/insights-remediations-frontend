3bcb62cc458e18ba7b88b0099353063db54d58ca
Adonis Puente <adonis.puente@gmail.com>
2025-07-29 12:32:10 -0500

    fix(RHINENG-18973): Reload after deleting an action

--- src/components/Modals/Columns.js
+++ src/components/Modals/Columns.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   ConnectionTypeCell,
   SystemsCell,
@@ -6,30 +5,25 @@ import {
 } from './Cells.js';
 import { wrappable } from '@patternfly/react-table';
 
-// eslint-disable-next-line react/display-name
-export const renderComponent = (Component, props) => (_data, _id, entity) => (
-  <Component {...entity} {...props} />
-);
-
 export const Systems = {
   title: 'Systems',
   transforms: [wrappable],
   sortable: 'system_count',
   exportKey: 'system_count',
-  renderFunc: renderComponent(SystemsCell),
+  Component: SystemsCell,
 };
 
 export const ConnectionType = {
   title: 'Connection type',
   transforms: [wrappable],
   exportKey: 'connection_type',
-  renderFunc: renderComponent(ConnectionTypeCell),
+  Component: ConnectionTypeCell,
 };
 
 export const ConnectionStatus = {
   title: 'Connection status',
   transforms: [wrappable],
   exportKey: 'connection_status',
-  renderFunc: renderComponent(ConnectionStatusCell),
+  Component: ConnectionStatusCell,
 };
 export default [ConnectionType, Systems, ConnectionStatus];
