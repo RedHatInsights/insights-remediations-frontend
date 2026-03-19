6ad48364e24cd6f546a7660328e9a1c2f8688152
Adonis Puente <adonis.puente@gmail.com>
2026-03-19 14:30:30 -0500

    refactor(cleanup): remove unused files

---
+++ src/modules/RemediationsModal/index.js
@@ -0,0 +1,5 @@
+/**
+ * Federated / legacy entry for the remediation wizard.
+ * Re-exports v2 so module federation and old resolve paths keep working.
+ */
+export { default } from '../../components/RemediationWizardV2/RemediationWizardV2';
